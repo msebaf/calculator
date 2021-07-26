f4f842230c5f08667c227d7e2db9df2a43bed7bf
msebaf <msebaf@hotmail.com>
2021-07-26 12:44:12 -0300

    basic operations not usable  UI, Done Html not yet properly styled

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+function sum(num1, num2){
+
+    return num1+num2;
+    
+    }
+    
+    function rest(num1,num2){
+        return num1-num2;
+    }
+    
+    function multi(num1,num2){
+        return num1*num2;
+    }
+    
+    function divi(num1,num2){
+        return num1/num2;
+    }
